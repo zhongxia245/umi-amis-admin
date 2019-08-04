55ab8bf900fdae28b9da7bac45c94b0288d9e562
zhongxia <huangxiaodong@licaigc.com>
2019-08-05 00:35:28 +0800

    feature: layout 改成 antd-pro 的布局

---
+++ src/config/menu.ts
@@ -0,0 +1,86 @@
+/* eslint no-useless-escape:0 */
+const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/g;
+
+export function isUrl(path: string) {
+  return reg.test(path);
+}
+
+const menuData = [
+  {
+    name: 'DashBoard',
+    icon: 'dashboard',
+    path: 'dashboard',
+    children: [
+      {
+        name: '工作台',
+        path: '/',
+      },
+      {
+        name: '分析页',
+        path: 'analysis',
+      },
+      {
+        name: '监控页',
+        path: 'monitor',
+      },
+    ],
+  },
+  {
+    name: '应用管理',
+    icon: 'appstore',
+    path: 'system/app',
+    children: [
+      {
+        name: '生成应用',
+        path: '1',
+      },
+      {
+        name: '创建应用',
+        path: 'create',
+      },
+      {
+        name: '应用列表',
+        path: 'list',
+      },
+      {
+        name: '应用分组',
+        path: 'group',
+      },
+    ],
+  },
+  {
+    name: '用户管理',
+    icon: 'user',
+    path: 'system/user',
+    children: [
+      {
+        name: '用户列表',
+        path: 'list',
+      },
+      {
+        name: '新建用户',
+        path: 'create',
+      },
+    ],
+  },
+];
+
+function formatter(data: any[], parentPath: string = '/', parentAuthority?: any) {
+  return data.map(item => {
+    let { path } = item;
+    if (!isUrl(path)) {
+      path = parentPath + item.path;
+    }
+    const result = {
+      ...item,
+      path,
+      authority: item.authority || parentAuthority,
+    };
+    if (item.children) {
+      result.children = formatter(item.children, `${parentPath}${item.path}/`, item.authority);
+    }
+    return result;
+  });
+}
+
+export const getMenuData = () => formatter(menuData);
--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -1,5 +1,5 @@
 import 'jest';
-import Index from '..';
+import Index from '../dashboard';
 import React from 'react';
 import renderer, { ReactTestInstance, ReactTestRenderer } from 'react-test-renderer';
 
@@ -11,6 +11,6 @@ describe('Page: index', () => {
     const outerLayer = wrapper.root.children[0] as ReactTestInstance;
     expect(outerLayer.type).toBe('div');
     expect(outerLayer.children.length).toBe(2);
-    
+
   });
 });
